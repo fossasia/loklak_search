89d5fd2d8c1780f8105e5bd160ca3e976969d76d
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-10-09 17:50:22 +0530

    Fixes #12: Search results page with deploy script
    Continuous Deployment using Travis
    
    * Deployment script that deploys the build on gh-pages.
    
    * Deploys only if build is on master
    
    * Deploy commit doesn't get pushed if nothing has changed
    
    * Cleans Up gh-pages every time
    
    Basic Feed component
    
    * Search service loading data for the component.
    
    * Feed displayed in very basic form.

--- src/app/feed/feed.component.spec.ts
+++ src/app/feed/feed.component.spec.ts
@@ -1,11 +1,33 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { Route } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { RouterStub } from '../../testing';
 import { FeedComponent } from './feed.component';
+import { SearchService } from '../shared/services';
+
+class SearchServiceStub { }
 
 describe('Component: Feed', () => {
-  it('should create an instance', () => {
-    let component = new FeedComponent();
-    expect(component).toBeTruthy();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule
+			],
+			declarations: [
+				FeedComponent,
+			],
+			providers: [
+				{ provide: SearchService, useClass: SearchServiceStub }
+			]
+		});
+	});
+
+	it('should create an instance', () => {
+		let fixture = TestBed.createComponent(FeedComponent);
+		let component = fixture.debugElement.componentInstance;
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { SearchService } from '../shared/services';
 
 @Component({
-  selector: 'app-feed',
-  templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+	selector: 'app-feed',
+	templateUrl: './feed.component.html',
+	styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
+	private query: string = null;
+	private search_metadata: JSON = null;	// Temporarily as a JSON in future it will be a model object
+	private search_results: JSON[] = null; // Temporarily as a JSON in future it will be a model object
 
-  constructor() { }
+	constructor(
+		private router: Router,
+		private route: ActivatedRoute,
+		private searchService: SearchService
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getQueryString();
+		this.loadResults();
+	}
 
+	private getQueryString() {
+		this.route.params.subscribe((params: Params) => {
+				this.query = params['query'];
+			});
+	}
+
+	private loadResults() {
+		this.searchService.fetchQuery(this.query)
+											.subscribe((fetchedResults: JSON) => {
+												this.search_metadata = fetchedResults['search_metadata'];
+												this.search_results = fetchedResults['statuses'];
+												console.log(this.search_results);
+											});
+	}
 }
