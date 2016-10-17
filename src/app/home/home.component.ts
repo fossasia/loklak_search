54e6809cec47c518357b54b6660f9935d70c0063
Hemant Jadon <hemantjadon.hemu@gmail.com>
2016-10-17 13:48:55 +0530

    Fixes #38: Added key escaping for firefox
    * Changed the event from keypress to keydown
    
    * Escaped the undesired keys.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,9 +17,55 @@ export class HomeComponent implements OnInit {
 	ngOnInit() { }
 
 	private handleTypeStart(event: KeyboardEvent): void {
-		if (event.charCode === 13) {	// Early exit if Enter is pressed
+		// Keypress events not working well with Firefox during the switching of the page from home to feed.
+		// Thus we need to use keydown event and then filter out only the "Character Keys"
+		// This filtering process can be extended till more precise keypress event.
+		// Please update this list as the project progresses.
+
+		switch (event.keyCode) {
+			case 8:  // Backspace / Mac Delete
+			case 9:  // Tab
+			case 13: // Enter
+			case 14: // Return (Gecko old) [Obsolete]
+			case 16: // Shift
+			case 17: // Ctrl
+			case 18: // Alt
+			case 19: // Pause/Break
+			case 20: // Caps Lock
+			case 27: // Escape
+			case 32: // Space
+			case 33: // Pg Up
+			case 34: // Pg Down
+			case 35: // End
+			case 36: // Home
+			case 37: // Left
+			case 38: // Up
+			case 39: // Right
+			case 40: // Down
+			case 41: // Linux Select
+			case 44: // Print Screen
+			case 45: // Insert
+			case 46: // Delete
+			case 93: // Context Menu
+			case 144: // Num Lock
+			case 145: // Scroll Lock
+
+			// Mac CMD Key
+			case 91: // Safari, Chrome
+			case 93: // Safari, Chrome
+			case 224: // Firefox
+
+			// Mac Num Lock
+			case 12:
+				return;
+			default:
+				break;
+		}
+
+		if (event.keyCode >= 112 && event.keyCode <= 135) { // Function keys (F1-F24)
 			return;
 		}
+
 		this.router.navigateByUrl(`/search`, { skipLocationChange: true });
 	}
 }
