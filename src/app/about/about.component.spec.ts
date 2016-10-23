441910e2d54edf26deca6c24ecb9cee1c51b5156
Rishi Raj <rishiraj824@gmail.com>
2016-10-23 16:49:52 +0530

    #60 #61 errors resolved

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,16 +1,40 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AboutComponent } from './about.component';
 
+import { RouterStub } from '../../testing';
 @Component({
 	selector: 'app-footer',
 	template: ''
 })
+class FooterComponent { }
 
 describe('Component: About', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule
+			],
+			declarations: [
+				AboutComponent,
+				FooterComponent
+			],
+			providers: [
+				{ provide: Router, useClass: RouterStub }
+			]
+		});
+	});
+
 	it('should create an instance', () => {
 		let component = new AboutComponent();
 		expect(component).toBeTruthy();
 	});
 });
+
+
