6c114f262cba6dc7262e9345bb6ce18a3b658f7e
pranay <pranayyelugam.com>
2016-11-08 21:27:42 +0530

    fixes #103 implemented date/tme information

--- src/app/feed/feed-card/feed-card.component.ts
+++ src/app/feed/feed-card/feed-card.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiResponseResult } from '../../shared/classes';
-
+import {Observable} from 'rxjs/Rx';
 @Component({
 	selector: 'feed-card',
 	templateUrl: './feed-card.component.html',
 	styleUrls: ['./feed-card.component.scss']
 })
 export class FeedCardComponent implements OnInit {
+	 since=""; 
 	@Input() private feedItem: ApiResponseResult;
 
-	constructor() { }
-
-	ngOnInit() { }
-
+	constructor() {}
+	
+	ngOnInit(){
+	let timer = Observable.timer(0,10000);//interval setting
+    timer.subscribe(t => this.ttt());
+	}
 	private get profileURL(): string {
 		return `https://twitter.com/${this.feedItem.screen_name}/`;
 	}
@@ -63,4 +66,54 @@ export class FeedCardComponent implements OnInit {
 			}
 		}
 	}
+
+	private  ttt():any{
+		this.since=this.tdiff();
+	}
+
+	private convertDateToUTC(date) {
+	 	return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(),
+		  date.getUTCMinutes(), date.getUTCSeconds()); }
+
+	private  tdiff(): string {
+	
+	var createdadt=this.feedItem.created_at;
+	var today=new Date().toISOString(); //current time  in ISO format
+	var todaytime=new Date(today).getTime(); //current time in ms
+	var tweetday=new Date(createdadt).toISOString(); //tweeted time  in TSO format
+	var tweettime=new Date(tweetday).getTime();  //tweeted at in ms
+	var tt=todaytime-tweettime;  //(current time-tweeted at) in ms
+	var sinceMin=Math.floor(Math.abs((todaytime-tweettime))/60000);
+	var now=(new Date()).getFullYear();
+	var months = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' ');
+	var date2 = new Date(createdadt);
+    if(sinceMin==0){
+        var sinceSec=Math.round((todaytime-tweettime)/1000);
+		if(sinceSec<=24){
+			var since="now";
+		}
+		else{
+			var since=sinceSec+"s";
+		}
+    }
+    else if(sinceMin>=1&&sinceMin<60){
+        var since=sinceMin+'m';
+    }
+	else if(sinceMin<1440){
+        var sinceHr=Math.round(sinceMin/60);
+      	var since=sinceHr+'h';
+	}
+    else if(date2.getFullYear()==now){
+		var since= months[date2.getMonth()] +' '+date2.getDate()  ;
+	}
+	else{
+		var since= date2.getDate()+' '+months[date2.getMonth()]+' '+date2.getFullYear()  ;
+	}
+    return since;
+	}
+
+
 }
+
+
+
