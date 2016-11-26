4db921be13b81f9e7bc1dc636b9036a38f426a24
Achint08 <gr8achint.sharma@gmail.com>
2016-11-26 17:53:57 +0530

    autoscroll when bottom links clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Loklak Search!';
+    constructor(private router: Router) {
+    router.events.subscribe((val) => {
+     if (val instanceof NavigationEnd){
+     window.scrollTo(0,0);
+     }
+     });
+     }
 }
