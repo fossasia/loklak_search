46e2c3f110a1d45f51fa80b968186821a9f0ba5d
Achint08 <gr8achint.sharma@gmail.com>
2016-11-26 19:44:22 +0530

    autoscroll when bottom links clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Router, NavigationEnd} from "@angular/router";
+import {Router, NavigationEnd} from '@angular/router';
 
 @Component({
 	selector: 'app-root',
