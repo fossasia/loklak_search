43d43b14f6b99fc5bb2e6127d16359b37171b8cb
Achint08 <gr8achint.sharma@gmail.com>
2017-01-14 06:30:42 +0530

    404 pages change
    404 pages
    
    404 change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+		path: 'about',
+		pathMatch: 'full',
+		component: AboutComponent
+	},
+	{
+		path: 'contact',
+		pathMatch: 'full',
+		component: ContactComponent
+	},
+	{
+		path: 'search',
+		component: FeedComponent
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		component: HomeComponent
+	},
+	{
+		path: 'terms',
+		pathMatch: 'full',
+		component: TermsComponent
+	},
+	{
+		path:'**',
+		redirectTo:''
+	}
+	];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
