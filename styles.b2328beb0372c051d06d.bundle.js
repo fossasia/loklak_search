46516e313a0447df42aff6b581fb7054610505aa
yasirunilan <yasiru.13@cse.mrt.ac.lk>
2017-01-27 22:35:26 -0800

    deploying after adding animation

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{1054:function(n,o,c){n.exports=c(466)},466:function(n,o){}},[1054]);
+webpackJsonp([1,2],{1062:function(n,o,c){n.exports=c(467)},467:function(n,o){}},[1062]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
