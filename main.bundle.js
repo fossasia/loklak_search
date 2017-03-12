d59164b157885b53f042ac3268706b931e33175a
Travis CI User <travis@example.org>
2017-03-12 02:22:41 +0000

    Travis CI Clean Deploy : 3f0b14ad6039d353965c63b82e8afcabf67ca10f

--- main.bundle.js
+++ main.bundle.js
@@ -71718,7 +71718,7 @@ module.exports = "footer {\n  width: 100%;\n  background: #f2f2f2;\n  padding: 0
 /* 802 */
 /***/ function(module, exports) {
 
-module.exports = ":host {\n  width: 100%;\n  height: 100%;\n  min-height: 100vh;\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -ms-flex-pack: distribute;\n      justify-content: space-around;\n  -webkit-box-align: center;\n      -ms-flex-align: center;\n          align-items: center; }\n\ndiv.wrapper {\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -webkit-box-orient: vertical;\n  -webkit-box-direction: normal;\n      -ms-flex-direction: column;\n          flex-direction: column;\n  -ms-flex-pack: distribute;\n      justify-content: space-around;\n  -webkit-box-align: center;\n      -ms-flex-align: center;\n          align-items: center;\n  width: 500px;\n  margin-top: -30vh; }\n  div.wrapper img {\n    max-width: 25%;\n    height: auto; }\n  div.wrapper h1 {\n    font-weight: 300;\n    text-align: center;\n    font-size: 2.5em;\n    margin: 15px 0px 0px 0px;\n    font-family: 'Roboto', sans-serif; }\n    div.wrapper h1 .loklak {\n      background: #f9f2f4;\n      color: #d10042;\n      font-family: Courier New; }\n  div.wrapper h5 {\n    text-align: center; }\n  div.wrapper div.search-form {\n    background-color: #fff;\n    height: 44px;\n    vertical-align: top;\n    border-radius: 2px;\n    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);\n    margin: 20px 0px 20px 0;\n    min-width: 584px;\n    border: 0 !important;\n    outline: 0 !important;\n    -webkit-transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);\n    transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1); }\n    div.wrapper div.search-form div.input-group {\n      border: 0 !important;\n      height: 44px;\n      width: 100%; }\n      div.wrapper div.search-form div.input-group label {\n        visibility: hidden;\n        display: none; }\n      div.wrapper div.search-form div.input-group input {\n        padding: 10px 15px;\n        font-size: 16px;\n        word-spacing: 1.5pt;\n        letter-spacing: 0.5pt;\n        box-shadow: none;\n        width: 100%;\n        border: 0 !important;\n        outline: 0 !important;\n        font: 16px arial,sans-serif;\n        line-height: 44px;\n        height: 44px !important;\n        min-width: 440px;\n        background: #FFFFFF !important; }\n      div.wrapper div.search-form div.input-group input:focus {\n        box-shadow: none;\n        border-color: none;\n        cursor: text; }\n      div.wrapper div.search-form div.input-group span.input-group-btn button {\n        height: 40px;\n        background: transparent;\n        cursor: pointer; }\n      div.wrapper div.search-form div.input-group input {\n        cursor: text; }\n  div.wrapper div.search-form:hover {\n    box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08); }\n  div.wrapper .help-text {\n    font-size: 10pt;\n    text-align: center; }\n    div.wrapper .help-text .highlight {\n      background-color: #eeeeee;\n      border-radius: 6px;\n      padding: 1px 3px; }\n  div.wrapper app-footer {\n    position: absolute;\n    bottom: 0;\n    left: 0;\n    right: 0; }\n\n@media screen and (max-width: 880px) {\n  div.wrapper div.search-form {\n    min-width: 260px;\n    margin: 20px 15px 20px 15px; }\n    div.wrapper div.search-form input {\n      min-width: 230px !important; } }\n"
+module.exports = ":host {\n  width: 100%;\n  min-height: 100vh;\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -ms-flex-pack: distribute;\n      justify-content: space-around;\n  -webkit-box-align: center;\n      -ms-flex-align: center;\n          align-items: center;\n  position: relative; }\n\ndiv.wrapper {\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -webkit-box-orient: vertical;\n  -webkit-box-direction: normal;\n      -ms-flex-direction: column;\n          flex-direction: column;\n  -ms-flex-pack: distribute;\n      justify-content: space-around;\n  -webkit-box-align: center;\n      -ms-flex-align: center;\n          align-items: center;\n  width: 500px;\n  margin-bottom: 50px; }\n  div.wrapper img {\n    max-width: 25%;\n    height: auto; }\n  div.wrapper h1 {\n    font-weight: 300;\n    text-align: center;\n    font-size: 2.5em;\n    margin: 15px 0px 0px 0px;\n    font-family: 'Roboto', sans-serif; }\n    div.wrapper h1 .loklak {\n      background: #f9f2f4;\n      color: #d10042;\n      font-family: Courier New; }\n  div.wrapper h5 {\n    text-align: center; }\n  div.wrapper div.search-form {\n    background-color: #fff;\n    height: 44px;\n    vertical-align: top;\n    border-radius: 2px;\n    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);\n    margin: 20px 0px 20px 0;\n    min-width: 584px;\n    border: 0 !important;\n    outline: 0 !important;\n    -webkit-transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);\n    transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1); }\n    div.wrapper div.search-form div.input-group {\n      border: 0 !important;\n      height: 44px;\n      width: 100%; }\n      div.wrapper div.search-form div.input-group label {\n        visibility: hidden;\n        display: none; }\n      div.wrapper div.search-form div.input-group input {\n        padding: 10px 15px;\n        font-size: 16px;\n        word-spacing: 1.5pt;\n        letter-spacing: 0.5pt;\n        box-shadow: none;\n        width: 100%;\n        border: 0 !important;\n        outline: 0 !important;\n        font: 16px arial,sans-serif;\n        line-height: 44px;\n        height: 44px !important;\n        min-width: 440px;\n        background: #FFFFFF !important; }\n      div.wrapper div.search-form div.input-group input:focus {\n        box-shadow: none;\n        border-color: none;\n        cursor: text; }\n      div.wrapper div.search-form div.input-group span.input-group-btn button {\n        height: 40px;\n        background: transparent;\n        cursor: pointer; }\n      div.wrapper div.search-form div.input-group input {\n        cursor: text; }\n  div.wrapper div.search-form:hover {\n    box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08); }\n  div.wrapper .help-text {\n    font-size: 10pt;\n    text-align: center; }\n    div.wrapper .help-text .highlight {\n      background-color: #eeeeee;\n      border-radius: 6px;\n      padding: 1px 3px; }\n  div.wrapper app-footer {\n    position: absolute;\n    bottom: 0;\n    width: 100vw; }\n\n@media screen and (max-width: 880px) {\n  div.wrapper div.search-form {\n    min-width: 260px;\n    margin: 20px 15px 20px 15px; }\n    div.wrapper div.search-form input {\n      min-width: 230px !important; } }\n"
 
 /***/ },
 /* 803 */
@@ -83511,9 +83511,9 @@ exports.toSubscriber = toSubscriber;
 * found in the LICENSE file at https://angular.io/license
 */
 (function (global, factory) {
-     true ? factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (factory());
+	 true ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(factory());
 }(this, (function () { 'use strict';
 
 /**
@@ -83678,7 +83678,11 @@ var Zone$1 = (function (global) {
             }
             finally {
                 if (task.type == eventTask || (task.data && task.data.isPeriodic)) {
-                    reEntryGuard && task._transitionTo(scheduled, running, notScheduled);
+                    // if the task's state is notScheduled, then it has already been cancelled
+                    // we should not reset the state to scheduled
+                    if (task.state !== notScheduled) {
+                        reEntryGuard && task._transitionTo(scheduled, running);
+                    }
                 }
                 else {
                     task.runCount = 0;
@@ -83946,7 +83950,6 @@ var Zone$1 = (function (global) {
                 }
             }
             else {
-                debugger;
                 throw new Error(this.type + " '" + this.source + "': can not transition to '" + toState + "', expecting state '" + fromState1 + "'" + (fromState2 ?
                     ' or \'' + fromState2 + '\'' :
                     '') + ", was '" + this._state + "'.");
@@ -84016,11 +84019,25 @@ var Zone$1 = (function (global) {
         _microTaskQueue.push(task);
     }
     function consoleError(e) {
+        if (Zone[__symbol__('ignoreConsoleErrorUncaughtError')]) {
+            return;
+        }
         var rejection = e && e.rejection;
         if (rejection) {
             console.error('Unhandled Promise rejection:', rejection instanceof Error ? rejection.message : rejection, '; Zone:', e.zone.name, '; Task:', e.task && e.task.source, '; Value:', rejection, rejection instanceof Error ? rejection.stack : undefined);
         }
         console.error(e);
+    }
+    function handleUnhandledRejection(e) {
+        consoleError(e);
+        try {
+            var handler = Zone[__symbol__('unhandledPromiseRejectionHandler')];
+            if (handler && typeof handler === 'function') {
+                handler.apply(this, [e]);
+            }
+        }
+        catch (err) {
+        }
     }
     function drainMicroTaskQueue() {
         if (!_isDrainingMicrotaskQueue) {
@@ -84047,7 +84064,7 @@ var Zone$1 = (function (global) {
                         });
                     }
                     catch (error) {
-                        consoleError(error);
+                        handleUnhandledRejection(error);
                     }
                 };
                 while (_uncaughtPromiseErrors.length) {
@@ -84139,6 +84156,11 @@ var Zone$1 = (function (global) {
                 promise[symbolState] = state;
                 var queue = promise[symbolValue];
                 promise[symbolValue] = value;
+                // record task information in value when error occurs, so we can
+                // do some additional work such as render longStackTrace
+                if (state === REJECTED && value instanceof Error) {
+                    value[__symbol__('currentTask')] = Zone.currentTask;
+                }
                 for (var i = 0; i < queue.length;) {
                     scheduleResolveOrReject(promise, queue[i++], queue[i++], queue[i++], queue[i++]);
                 }
@@ -84165,11 +84187,23 @@ var Zone$1 = (function (global) {
     }
     function clearRejectedNoCatch(promise) {
         if (promise[symbolState] === REJECTED_NO_CATCH) {
+            // if the promise is rejected no catch status
+            // and queue.length > 0, means there is a error handler
+            // here to handle the rejected promise, we should trigger
+            // windows.rejectionhandled eventHandler or nodejs rejectionHandled
+            // eventHandler
+            try {
+                var handler = Zone[__symbol__('rejectionHandledHandler')];
+                if (handler && typeof handler === 'function') {
+                    handler.apply(this, [{ rejection: promise[symbolValue], promise: promise }]);
+                }
+            }
+            catch (err) {
+            }
             promise[symbolState] = REJECTED;
             for (var i = 0; i < _uncaughtPromiseErrors.length; i++) {
                 if (promise === _uncaughtPromiseErrors[i].promise) {
                     _uncaughtPromiseErrors.splice(i, 1);
-                    break;
                 }
             }
         }
@@ -84507,6 +84541,24 @@ var Zone$1 = (function (global) {
     ZoneAwareError.prototype = NativeError.prototype;
     ZoneAwareError[Zone.__symbol__('blacklistedStackFrames')] = blackListedStackFrames;
     ZoneAwareError[stackRewrite] = false;
+    // those properties need special handling
+    var specialPropertyNames = ['stackTraceLimit', 'captureStackTrace', 'prepareStackTrace'];
+    // those properties of NativeError should be set to ZoneAwareError
+    var nativeErrorProperties = Object.keys(NativeError);
+    if (nativeErrorProperties) {
+        nativeErrorProperties.forEach(function (prop) {
+            if (specialPropertyNames.filter(function (sp) { return sp === prop; }).length === 0) {
+                Object.defineProperty(ZoneAwareError, prop, {
+                    get: function () {
+                        return NativeError[prop];
+                    },
+                    set: function (value) {
+                        NativeError[prop] = value;
+                    }
+                });
+            }
+        });
+    }
     if (NativeError.hasOwnProperty('stackTraceLimit')) {
         // Extend default stack limit as we will be removing few frames.
         NativeError.stackTraceLimit = Math.max(NativeError.stackTraceLimit, 15);
@@ -84632,7 +84684,7 @@ var Zone$1 = (function (global) {
 /**
  * Suppress closure compiler errors about unknown 'Zone' variable
  * @fileoverview
- * @suppress {undefinedVars}
+ * @suppress {undefinedVars,globalThis}
  */
 var zoneSymbol = function (n) { return ("__zone_symbol__" + n); };
 var _global$1 = typeof window === 'object' && window || typeof self === 'object' && self || global;
@@ -84684,7 +84736,7 @@ function patchProperty(obj, prop) {
     delete desc.value;
     // substr(2) cuz 'onclick' -> 'click', etc
     var eventName = prop.substr(2);
-    var _prop = '_' + prop;
+    var _prop = zoneSymbol('_' + prop);
     desc.set = function (fn) {
         if (this[_prop]) {
             this.removeEventListener(eventName, this[_prop]);
@@ -84765,24 +84817,6 @@ function findExistingRegisteredTask(target, handler, name, capture, remove) {
                 return eventTask;
             }
         }
-    }
-    return null;
-}
-function findAllExistingRegisteredTasks(target, name, capture, remove) {
-    var eventTasks = target[EVENT_TASKS];
-    if (eventTasks) {
-        var result = [];
-        for (var i = eventTasks.length - 1; i >= 0; i--) {
-            var eventTask = eventTasks[i];
-            var data = eventTask.data;
-            if (data.eventName === name && data.useCapturing === capture) {
-                result.push(eventTask);
-                if (remove) {
-                    eventTasks.splice(i, 1);
-                }
-            }
-        }
-        return result;
     }
     return null;
 }
@@ -85013,6 +85047,25 @@ function patchMethod(target, name, patchFn) {
 }
 // TODO: @JiaLiPassion, support cancel task later if necessary
 
+
+function findEventTask(target, evtName) {
+    var eventTasks = target[zoneSymbol('eventTasks')];
+    var result = [];
+    if (eventTasks) {
+        for (var i = 0; i < eventTasks.length; i++) {
+            var eventTask = eventTasks[i];
+            var data = eventTask.data;
+            var eventName = data && data.eventName;
+            if (eventName === evtName) {
+                result.push(eventTask);
+            }
+        }
+    }
+    return result;
+}
+Zone[zoneSymbol('patchEventTargetMethods')] = patchEventTargetMethods;
+Zone[zoneSymbol('patchOnProperties')] = patchOnProperties;
+
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -85029,8 +85082,12 @@ function patchTimer(window, setName, cancelName, nameSuffix) {
     function scheduleTask(task) {
         var data = task.data;
         data.args[0] = function () {
-            task.invoke.apply(this, arguments);
-            delete tasksByHandleId[data.handleId];
+            try {
+                task.invoke.apply(this, arguments);
+            }
+            finally {
+                delete tasksByHandleId[data.handleId];
+            }
         };
         data.handleId = setNative.apply(window, data.args);
         tasksByHandleId[data.handleId] = task;
@@ -85452,7 +85509,9 @@ function patchXHR(window) {
         }
         var newListener = data.target[XHR_LISTENER] = function () {
             if (data.target.readyState === data.target.DONE) {
-                if (!data.aborted && self[XHR_SCHEDULED]) {
+                // sometimes on some browsers XMLHttpRequest will fire onreadystatechange with
+                // readyState=4 multiple times, so we need to check task state here
+                if (!data.aborted && self[XHR_SCHEDULED] && task.state === 'scheduled') {
                     task.invoke();
                 }
             }
@@ -85508,6 +85567,26 @@ function patchXHR(window) {
 /// GEO_LOCATION
 if (_global['navigator'] && _global['navigator'].geolocation) {
     patchPrototype(_global['navigator'].geolocation, ['getCurrentPosition', 'watchPosition']);
+}
+// handle unhandled promise rejection
+function findPromiseRejectionHandler(evtName) {
+    return function (e) {
+        var eventTasks = findEventTask(_global, evtName);
+        eventTasks.forEach(function (eventTask) {
+            // windows has added unhandledrejection event listener
+            // trigger the event listener
+            var PromiseRejectionEvent = _global['PromiseRejectionEvent'];
+            if (PromiseRejectionEvent) {
+                var evt = new PromiseRejectionEvent(evtName, { promise: e.promise, reason: e.rejection });
+                eventTask.invoke(evt);
+            }
+        });
+    };
+}
+if (_global['PromiseRejectionEvent']) {
+    Zone[zoneSymbol('unhandledPromiseRejectionHandler')] =
+        findPromiseRejectionHandler('unhandledrejection');
+    Zone[zoneSymbol('rejectionHandledHandler')] = findPromiseRejectionHandler('rejectionhandled');
 }
 
 /**
