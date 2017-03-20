43340f21fbc8c33921216348a0d6dd34ea38e5ee
Travis CI User <travis@example.org>
2017-03-20 18:07:39 +0000

    Travis CI Clean Deploy : 4ba81cf92ce611723eaee33f4339bfbcb54c346e

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -68123,30 +68123,50 @@ function RouterPreloader_tsickle_Closure_declarations() {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_operator_filter__ = __webpack_require__(145);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_operator_filter___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_rxjs_operator_filter__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Actions; });
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+// TODO: This is a copy of this: https://github.com/redux-observable/redux-observable/blob/master/src/ActionsObservable.js
+// Remove after this is resolved: https://github.com/redux-observable/redux-observable/issues/95
 
 
 
 
-var Actions = (function (_super) {
+var Actions = Actions_1 = (function (_super) {
     __extends(Actions, _super);
     function Actions(actionsSubject) {
-        _super.call(this);
-        this.source = actionsSubject;
+        var _this = _super.call(this) || this;
+        _this.source = actionsSubject;
+        return _this;
     }
     Actions.prototype.lift = function (operator) {
-        var observable = new Actions(this);
+        var observable = new Actions_1(this);
         observable.operator = operator;
         return observable;
     };
     Actions.prototype.ofType = function () {
         var keys = [];
         for (var _i = 0; _i < arguments.length; _i++) {
-            keys[_i - 0] = arguments[_i];
+            keys[_i] = arguments[_i];
         }
         return __WEBPACK_IMPORTED_MODULE_3_rxjs_operator_filter__["filter"].call(this, function (_a) {
             var type = _a.type;
@@ -68164,15 +68184,15 @@ var Actions = (function (_super) {
             return false;
         });
     };
-    Actions.decorators = [
-        { type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"] },
-    ];
-    /** @nocollapse */
-    Actions.ctorParameters = [
-        { type: __WEBPACK_IMPORTED_MODULE_2_rxjs_Observable__["Observable"], decorators: [{ type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Inject"], args: [__WEBPACK_IMPORTED_MODULE_1__ngrx_store__["d" /* Dispatcher */],] },] },
-    ];
     return Actions;
 }(__WEBPACK_IMPORTED_MODULE_2_rxjs_Observable__["Observable"]));
+Actions = Actions_1 = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
+    __param(0, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Inject"])(__WEBPACK_IMPORTED_MODULE_1__ngrx_store__["d" /* Dispatcher */])),
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2_rxjs_Observable__["Observable"]])
+], Actions);
+
+var Actions_1;
 //# sourceMappingURL=actions.js.map
 
 /***/ }),
@@ -68189,10 +68209,27 @@ var Actions = (function (_super) {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__effects__ = __webpack_require__(391);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return effects; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EffectsSubscription; });
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
 };
 
 
@@ -68203,15 +68240,16 @@ var effects = new __WEBPACK_IMPORTED_MODULE_0__angular_core__["OpaqueToken"]('ng
 var EffectsSubscription = (function (_super) {
     __extends(EffectsSubscription, _super);
     function EffectsSubscription(store, parent, effectInstances) {
-        _super.call(this);
-        this.store = store;
-        this.parent = parent;
+        var _this = _super.call(this) || this;
+        _this.store = store;
+        _this.parent = parent;
         if (Boolean(parent)) {
-            parent.add(this);
+            parent.add(_this);
         }
         if (Boolean(effectInstances)) {
-            this.addEffects(effectInstances);
+            _this.addEffects(effectInstances);
         }
+        return _this;
     }
     EffectsSubscription.prototype.addEffects = function (effectInstances) {
         var sources = effectInstances.map(__WEBPACK_IMPORTED_MODULE_4__effects__["b" /* mergeEffects */]);
@@ -68223,17 +68261,16 @@ var EffectsSubscription = (function (_super) {
             this.unsubscribe();
         }
     };
-    EffectsSubscription.decorators = [
-        { type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"] },
-    ];
-    /** @nocollapse */
-    EffectsSubscription.ctorParameters = [
-        { type: undefined, decorators: [{ type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Inject"], args: [__WEBPACK_IMPORTED_MODULE_1__ngrx_store__["b" /* Store */],] },] },
-        { type: EffectsSubscription, decorators: [{ type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Optional"] }, { type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["SkipSelf"] },] },
-        { type: Array, decorators: [{ type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Optional"] }, { type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Inject"], args: [effects,] },] },
-    ];
     return EffectsSubscription;
 }(__WEBPACK_IMPORTED_MODULE_2_rxjs_Subscription__["Subscription"]));
+EffectsSubscription = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
+    __param(0, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Inject"])(__WEBPACK_IMPORTED_MODULE_1__ngrx_store__["b" /* Store */])),
+    __param(1, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Optional"])()), __param(1, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["SkipSelf"])()),
+    __param(2, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Optional"])()), __param(2, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Inject"])(effects)),
+    __metadata("design:paramtypes", [Object, EffectsSubscription, Array])
+], EffectsSubscription);
+
 //# sourceMappingURL=effects-subscription.js.map
 
 /***/ }),
@@ -89230,16 +89267,26 @@ function runAfterBootstrapEffects(injector, subscription) {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__effects_subscription__ = __webpack_require__(390);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__bootstrap_listener__ = __webpack_require__(603);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EffectsModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 
 
 
 
-var EffectsModule = (function () {
-    function EffectsModule() {
+var EffectsModule = EffectsModule_1 = (function () {
+    function EffectsModule(effectsSubscription) {
+        this.effectsSubscription = effectsSubscription;
     }
     EffectsModule.run = function (type) {
         return {
-            ngModule: EffectsModule,
+            ngModule: EffectsModule_1,
             providers: [
                 __WEBPACK_IMPORTED_MODULE_2__effects_subscription__["a" /* EffectsSubscription */],
                 type,
@@ -89249,31 +89296,32 @@ var EffectsModule = (function () {
     };
     EffectsModule.runAfterBootstrap = function (type) {
         return {
-            ngModule: EffectsModule,
+            ngModule: EffectsModule_1,
             providers: [
                 type,
                 { provide: __WEBPACK_IMPORTED_MODULE_3__bootstrap_listener__["a" /* afterBootstrapEffects */], useExisting: type, multi: true }
             ]
         };
     };
-    EffectsModule.decorators = [
-        { type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"], args: [{
-                    providers: [
-                        __WEBPACK_IMPORTED_MODULE_1__actions__["a" /* Actions */],
-                        __WEBPACK_IMPORTED_MODULE_2__effects_subscription__["a" /* EffectsSubscription */],
-                        {
-                            provide: __WEBPACK_IMPORTED_MODULE_0__angular_core__["APP_BOOTSTRAP_LISTENER"],
-                            multi: true,
-                            deps: [__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injector"], __WEBPACK_IMPORTED_MODULE_2__effects_subscription__["a" /* EffectsSubscription */]],
-                            useFactory: __WEBPACK_IMPORTED_MODULE_3__bootstrap_listener__["b" /* runAfterBootstrapEffects */]
-                        }
-                    ]
-                },] },
-    ];
-    /** @nocollapse */
-    EffectsModule.ctorParameters = [];
     return EffectsModule;
 }());
+EffectsModule = EffectsModule_1 = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        providers: [
+            __WEBPACK_IMPORTED_MODULE_1__actions__["a" /* Actions */],
+            __WEBPACK_IMPORTED_MODULE_2__effects_subscription__["a" /* EffectsSubscription */],
+            {
+                provide: __WEBPACK_IMPORTED_MODULE_0__angular_core__["APP_BOOTSTRAP_LISTENER"],
+                multi: true,
+                deps: [__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injector"], __WEBPACK_IMPORTED_MODULE_2__effects_subscription__["a" /* EffectsSubscription */]],
+                useFactory: __WEBPACK_IMPORTED_MODULE_3__bootstrap_listener__["b" /* runAfterBootstrapEffects */]
+            }
+        ]
+    }),
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__effects_subscription__["a" /* EffectsSubscription */]])
+], EffectsModule);
+
+var EffectsModule_1;
 //# sourceMappingURL=effects.module.js.map
 
 /***/ }),
