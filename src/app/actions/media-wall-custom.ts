197cdb83f0bbe9386cd28f8b4c46342ebf2236a9
Achint08 <gr8achint.sharma@gmail.com>
2017-08-08 20:26:08 +0530

    Fixes #504: Introduces pre-defined themes in media wall

--- src/app/actions/media-wall-custom.ts
+++ src/app/actions/media-wall-custom.ts
@@ -12,7 +12,11 @@ import { WallHeader, WallBackground, WallCard } from '../models';
 export const ActionTypes = {
 	WALL_HEADER_PROPERTIES_CHANGE: '[Media Wall] Media Wall Header Properties Change',
 	WALL_BACKGROUND_PROPERTIES_CHANGE: '[Media Wall] Media Wall Background Properties Change',
-	WALL_CARD_PROPERTIES_CHANGE: '[Media Wall] Media Wall Card Properties Change'
+	WALL_CARD_PROPERTIES_CHANGE: '[Media Wall] Media Wall Card Properties Change',
+	WALL_LIGHT_THEME_CHANGE: '[Media Wall] Media Wall Basic Theme',
+	WALL_DARK_THEME_CHANGE: '[Media Wall] Media Wall Dark Theme',
+	WALL_CLASSIC_THEME_CHANGE: '[Media Wall] Media Wall Classic Theme',
+	WALL_COOL_BLUES_THEME_CHANGE: '[Media Wall] Media Wall Cool Blues Theme'
 };
 
 /**
@@ -41,6 +45,30 @@ export class WallCardPropertiesChangeAction implements Action {
 	constructor(public payload: WallCard) { }
 }
 
+export class WallLightThemeChangeAction implements Action {
+	type = ActionTypes.WALL_LIGHT_THEME_CHANGE;
+
+	constructor(public payload: '') { }
+}
+
+export class WallClassicThemeChangeAction implements Action {
+	type = ActionTypes.WALL_CLASSIC_THEME_CHANGE;
+
+	constructor(public payload: '') { }
+}
+
+export class WallDarkThemeChangeAction implements Action {
+	type = ActionTypes.WALL_DARK_THEME_CHANGE;
+
+	constructor(public payload: '') { }
+}
+
+export class WallCoolBluesThemeChangeAction implements Action {
+	type = ActionTypes.WALL_COOL_BLUES_THEME_CHANGE;
+
+	constructor(public payload: '') { }
+}
+
 /**
  * Export a type alias of all actions in this action group
  * so that reducers can easily compose action types
@@ -48,4 +76,8 @@ export class WallCardPropertiesChangeAction implements Action {
 export type Actions
 	= WallBackgroundPropertiesChangeAction
 	| WallCardPropertiesChangeAction
-	| WallHeaderPropertiesChangeAction;
+	| WallHeaderPropertiesChangeAction
+	| WallClassicThemeChangeAction
+	| WallLightThemeChangeAction
+	| WallCoolBluesThemeChangeAction
+	| WallDarkThemeChangeAction;
