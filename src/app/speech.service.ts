11b45b8779660aa0377b3037fa0a80045da13461
Saurabh Srivastava <saurabhsri.sri123@gmail.com>
2018-03-15 23:20:03 +0530

    Fixed #674: Stop(deactivate) voice search when completed (#675)
    * Deactivate Speech on completion
    
    * Fix Lint Issue

--- src/app/speech.service.ts
+++ src/app/speech.service.ts
@@ -8,23 +8,30 @@ interface IWindow extends Window {
 @Injectable()
 export class SpeechService {
 
+	recognition: any;
+
 	constructor(private zone: NgZone) { }
 
 	record(lang: string): Observable<string> {
 				return Observable.create(observe => {
 						const { webkitSpeechRecognition }: IWindow = <IWindow>window;
-						const recognition = new webkitSpeechRecognition();
+						this.recognition = new webkitSpeechRecognition();
 
-						recognition.continuous = true;
-						recognition.interimResults = true;
-						recognition.onresult = take => this.zone.run(() =>
+						this.recognition.continuous = true;
+						this.recognition.interimResults = true;
+						this.recognition.onresult = take => this.zone.run(() =>
 								observe.next(take.results.item(take.results.length - 1).item(0).transcript)
 						);
 
-						recognition.onerror = err => observe.error(err);
-						recognition.onend = () => observe.complete();
-						recognition.lang = lang;
-						recognition.start();
+						this.recognition.onerror = err => observe.error(err);
+						this.recognition.onend = () => observe.complete();
+						this.recognition.lang = lang;
+						this.recognition.start();
 				});
 		}
+	stoprecord() {
+		if (this.recognition) {
+			this.recognition.stop();
+		}
+	}
 }
