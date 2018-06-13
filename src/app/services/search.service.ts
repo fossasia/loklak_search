ef5ec9d68007d01f693bac91e354d27bff75e8ab
Saurabh Srivastava <saurabhsri.sri123@gmail.com>
2018-06-13 16:46:31 +0530

    Fixes #738: Fix 'No results after upgrading to latest version' (#749)

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,40 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, map, retry } from 'rxjs/operators';
+import { catchError, retry } from 'rxjs/operators';
 
 import { SearchServiceConfig } from '.';
 import { ApiResponse } from '../models/api-response';
 
 @Injectable()
 export class SearchService {
-	private static readonly apiUrl: URL = new URL('https://api.loklak.org/api/search.json');
 
 	constructor(
 		private http: HttpClient
 	) { }
 
 	public fetchQuery(query: string, config: SearchServiceConfig): Observable<ApiResponse> {
-		const searchParams = new HttpParams();
-		searchParams.set('q', query);
-		searchParams.set('callback', 'JSONP_CALLBACK');
-		searchParams.set('minified', 'true');
-		searchParams.set('source', config.source);
-		searchParams.set('maximumRecords', config.maximumRecords.toString());
-		searchParams.set('timezoneOffset', config.getTimezoneOffset());
-		searchParams.set('startRecord', config.startRecord.toString());
-
-		if (config.getAggregationFieldString()) {
-			searchParams.set('fields', config.getAggregationFieldString());
-			searchParams.set('limit', config.aggregationLimit.toString());
-		}
-
-		if (config.getFilterString()) {
-			searchParams.set('filter', config.getFilterString());
-		}
-
-		const jsonpUrl = SearchService.apiUrl.toString() + `?${searchParams.toString()}`;
-
+		const jsonpUrl = 'https://api.loklak.org/api/search.json' +
+							'?q=' + query +
+							'&minified=' + 'true' +
+							'&source=' + config.source +
+							'&maximumRecords=' + config.maximumRecords.toString() +
+							'&timezoneOffset=' + config.getTimezoneOffset() +
+							'&startRecord=' + config.startRecord.toString();
 		return this.http
 			.jsonp<ApiResponse>(jsonpUrl, 'callback')
 			.pipe(
