bba801f2723e9e27f6443b45dff753e7acf42302
sk9331657@gmail.com <sk9331657@gmail.com>
2018-11-15 14:38:59 +0530

    Redesigining Loklak Search Settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -9,6 +9,7 @@ import { newsOrgs } from '../shared/news-org';
 })
 export class SettingsComponent implements OnInit {
 	public urlObject = [];
+	public urlObjecttext = [];
 	public newsConfigOrgs = newsOrgs;
 	ngOnInit() {
 		let i = 0;
@@ -27,6 +28,34 @@ export class SettingsComponent implements OnInit {
 				this.urlObject.push(data);
 				i++;
 			}
+
+		}
+		this.urlObjecttext = this.urlObject;
+		for ( let k = 0 ; k < this.urlObjecttext.length ; k++) {
+			for ( let l = 0 ; l < this.urlObjecttext[k][1].length ; l++) {
+				if ( this.urlObjecttext[k][1][l].split('://').length > 1) {
+					this.urlObjecttext[k][1][l] = this.urlObjecttext[k][1][l].split('://')[1];
+				}
+			}
+		}
+		this.urlObject = [];
+		let p = 0;
+		for (const key in defaultUrlConfig) {
+			if (key) {
+				const data = [];
+				const value = defaultUrlConfig[key];
+				const data2 = [];
+				for (const key2 in value) {
+					if (key2) {
+						const value2 = value[key2];
+						data2.push(key2, value2);
+					}
+				}
+				data.push(key, data2);
+				this.urlObject.push(data);
+				p++;
+			}
+
 		}
 	}
 }
