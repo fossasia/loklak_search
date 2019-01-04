c5b7ee38fc296fc24dfad3808757d99d44e4706c
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-01-04 17:34:10 +0530

    Added tests for feed-info-box and feed-user-info-box component

--- src/app/feed/info-box/info-box.component.spec.ts
+++ src/app/feed/info-box/info-box.component.spec.ts
@@ -5,7 +5,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { StoreModule } from '@ngrx/store';
 import { InfoBoxComponent } from './info-box.component';
-import { reducers } from '../../reducers';
+import * as fromRoot from '../../reducers';
 
 describe('Component: InfoBox', () => {
 	beforeEach(() => {
@@ -13,7 +13,7 @@ describe('Component: InfoBox', () => {
 			imports: [
 				RouterTestingModule,
 				ChartsModule,
-				StoreModule.forRoot(reducers)
+				StoreModule.forRoot(fromRoot.reducers)
 			],
 			declarations: [
 				InfoBoxComponent
@@ -22,7 +22,83 @@ describe('Component: InfoBox', () => {
 	});
 
 	it('should create an instance', async(() => {
-		const component = TestBed.createComponent(InfoBoxComponent);
+		const fixture = TestBed.createComponent(InfoBoxComponent);
+		const component = fixture.debugElement.componentInstance;
 		expect(component).toBeTruthy();
+	}));
+
+	it('should give valid result on non-empty ApiResponseResult', async(() => {
+		const fixture = TestBed.createComponent(InfoBoxComponent);
+		const component = fixture.debugElement.componentInstance;
+
+		component.ApiResponseResult = [
+			{
+				hashtags: [
+					'FOSSASIA',
+					'opensource',
+					'pslab',
+					'35c3',
+					'madeinsingapore',
+					'FOSSASIA',
+					'pslab'
+				],
+				screen_name: [
+					'hpdang'
+				],
+				user: {
+					profile_image_url_https: 'https://pbs.twimg.com/profile_images/1141238022/fossasia-cubelogo_400x400.jpg',
+				},
+				mentions: [
+					'fossasia',
+					'mariobehling',
+					'hpdang',
+					'mariobehling'
+				],
+				created_at: [
+					new Date()
+				]
+			}
+		];
+		component.ngOnChanges();
+		expect(component.queryString).toBe('');
+		expect(component.topHashtags[0][0]).toBe('FOSSASIA');
+		expect(component.topMentions[0][0]).toBe('mariobehling');
+		expect(component.topFrequencyData).toBe(undefined);
+		expect(component.areFrequencyDataAvailable).toBe(true);
+		expect(component.topTwitterers.length).toBe(1);
+	}));
+
+	it('should give valid result on empty ApiResponseResult', async(() => {
+		const fixture = TestBed.createComponent(InfoBoxComponent);
+		const component = fixture.debugElement.componentInstance;
+
+		component.ApiResponseResult = [
+			{
+				hashtags: [],
+				screen_name: [],
+				user: {},
+				mentions: [],
+				created_at: []
+			}
+		];
+		component.ngOnChanges();
+		expect(component.queryString).toBe('');
+		expect(component.areTopHashtagsAvailable).toBe(undefined);
+		expect(component.areTopMentionsAvailable).toBe(false);
+		expect(component.topFrequencyData).toBe(undefined);
+		expect(component.areFrequencyDataAvailable).toBe(false);
+		expect(component.areTopTwitterersAvailable).toBe(undefined);
+	}));
+
+	it('should call inviewtwitters() and inviewmentions()', async(() => {
+		const fixture = TestBed.createComponent(InfoBoxComponent);
+		const component = fixture.debugElement.componentInstance;
+		const event = {
+			value: true
+		};
+		component.inviewtwitters(event);
+		component.inviewmentions(event);
+		expect(component.inviewporttwitters).toBe(true);
+		expect(component.inviewportmentions).toBe(true);
 	}));
 });
--- src/app/feed/user-info-box/user-info-box.component.spec.ts
+++ src/app/feed/user-info-box/user-info-box.component.spec.ts
@@ -1,8 +1,90 @@
 import { UserInfoBoxComponent } from './user-info-box.component';
+import { Component, Input, Output } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import * as fromRoot from '../../reducers';
+
+@Component({
+	selector: 'feed-linker',
+	template: ''
+})
+class FeedLinkerStubComponent {
+	@Input() text;
+	@Input() hashtags;
+	@Input() mentions;
+	@Input() links;
+	@Input() unshorten;
+	@Input() useAll;
+	@Output() showed;
+}
 
 describe('Component: UserInfoBox', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				StoreModule.forRoot(fromRoot.reducers)
+			],
+			declarations: [
+				UserInfoBoxComponent,
+				FeedLinkerStubComponent
+			]
+		});
+	});
+
 	it('should create an instance', () => {
-		const component = new UserInfoBoxComponent();
+		const fixture = TestBed.createComponent(UserInfoBoxComponent);
+		const component = fixture.debugElement.componentInstance;
 		expect(component).toBeTruthy();
 	});
+
+	it('should do something on ngOnInit', async(() => {
+		const fixture = TestBed.createComponent(UserInfoBoxComponent);
+		const component = fixture.debugElement.componentInstance;
+
+		component.apiResponseUserFollowing = [
+			{
+				username: 'user1',
+				followers_count: 5,
+				statuses_count: 10
+			},
+			{
+				username: 'user2',
+				followers_count: 2,
+				statuses_count: 15
+			},
+			{
+				username: 'user3',
+				followers_count: 10,
+				statuses_count: 5
+			},
+		];
+
+		component.apiResponseUserFollowers = [
+			{
+				username: 'userF1',
+				followers_count: 2,
+				statuses_count: 10
+			},
+			{
+				username: 'userF2',
+				followers_count: 2,
+				statuses_count: 15
+			},
+			{
+				username: 'userF3',
+				followers_count: 2,
+				statuses_count: 10
+			}
+		];
+		component.ngOnInit();
+		expect(component.sortedApiResponseUserFollowing[0].username).toBeTruthy('user3');
+		expect(component.sortedApiResponseUserFollowing[1].username).toBeTruthy('user1');
+		expect(component.sortedApiResponseUserFollowing[2].username).toBeTruthy('user2');
+
+		expect(component.sortedApiResponseUserFollowers[0].username).toBeTruthy('userF2');
+		expect(component.sortedApiResponseUserFollowers[1].username).toBeTruthy('userF1');
+		expect(component.sortedApiResponseUserFollowers[2].username).toBeTruthy('userF3');
+	}));
 });
