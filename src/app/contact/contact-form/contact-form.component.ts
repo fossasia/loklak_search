efc5b04482028e7e9aeab7e19e45cfb7874a9343
Aaryan-kapur <44099242+Aaryan-kapur@users.noreply.github.com>
2019-12-18 00:13:53 +0530

    edit-form-validation2

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { countrycodearray } from '../../shared/countrycode/countrycode';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {
+	FormGroup,
+	FormControl,
+	FormBuilder,
+	Validators
+} from "@angular/forms";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { countrycodearray } from "../../shared/countrycode/countrycode";
 
-@Component({
-	selector: 'contact-form',
-	templateUrl: './contact-form.component.html',
-	styleUrls: ['./contact-form.component.scss']
+@Component({selector: "contact-form", templateUrl: "./contact-form.component.html",styleUrls: ["./contact-form.component.scss"]
 })
 export class ContactFormComponent implements OnInit {
 	public contactForm: FormGroup;
@@ -15,16 +17,26 @@ export class ContactFormComponent implements OnInit {
 	public countries = countrycodearray;
 	@Output() hideContactForm: EventEmitter<any> = new EventEmitter();
 
-	constructor( private http: HttpClient ) {  		 }
+	constructor(private http: HttpClient) {}
 
 	ngOnInit() {
 		this.contactForm = new FormGroup({
-			name: new FormControl('', [<any>Validators.required]),
-			email: new FormControl('', [<any>Validators.required, <any>Validators.email]),
-			countrycode: new FormControl('', [<any>Validators.required]),
-			telephone: new FormControl('', [<any>Validators.required, <any>Validators.minLength(10), <any>Validators.pattern('^[0-9]*$')]),
-			message: new FormControl('', [<any>Validators.required, <any>Validators.minLength(100)])
-			});
+			name: new FormControl("", [<any>Validators.required]),
+			email: new FormControl("", [
+				<any>Validators.required,
+				<any>Validators.email
+			]),
+			countrycode: new FormControl("", [<any>Validators.required]),
+			telephone: new FormControl("", [
+				<any>Validators.required,
+				<any>Validators.minLength(10),
+				<any>Validators.pattern("^[0-9]*$")
+			]),
+			message: new FormControl("", [
+				<any>Validators.required,
+				<any>Validators.minLength(100)
+			])
+		});
 	}
 	public sendtosuperuser(user) {
 		this.hideContactForm.emit(false);
@@ -33,22 +45,25 @@ export class ContactFormComponent implements OnInit {
 		const data = JSON.stringify(formObj);
 		console.log(this.contactForm.value);
 
+		headers.append("Content-Type", "application/X-www-form-urlencoded");
+		headers.append("Accept", "application/json");
 
-		headers.append('Content-Type', 'application/X-www-form-urlencoded');
-		headers.append('Accept', 'application/json');
-
-		this.http.post('https://formspree.io/office@fossasia.org', data, { headers: headers })
-			.subscribe((response) => {
-				this.http.post('https://formspree.io/office@fossasia.org', data, { headers: headers })
-					.subscribe((responsesent) => {
-						console.log('Sent successfully');
+		this.http
+			.post("https://formspree.io/office@fossasia.org", data, {
+				headers: headers
+			})
+			.subscribe(response => {
+				this.http
+					.post("https://formspree.io/office@fossasia.org", data, {
+						headers: headers
+					})
+					.subscribe(responsesent => {
+						console.log("Sent successfully");
 					});
 			});
 	}
 
-
 	public showSubmit() {
 		this.submitted = true;
 	}
-
 }
